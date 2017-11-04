219e087792f8b4e4b92acb6d029d4a3c7335080e
ZoeyOneOhOne <cole.perry113@gmail.com>
2017-11-04 18:15:59 -0400

    Landing Page Done
    Finished

---
+++ Startup-Weekend-Application/Models/Discover.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Startup_Weekend_Application.Models.ManageViewModels;
+
+public class Discover
+    {
+        public int Id { get; set; }
+        public String Game { get; set; }
+        public String Platform { get; set; }
+        public String Playstyle { get; set; }
+        public Genre Genre { get; set; }
+    }
+
+public enum Genre
+{
+    FPS=1,
+    Fighting,
+    Survival,
+    Fantasy,
+    RPG,
+    MMORPG,
+    Sandbox,
+    Moba,
+    Strategy,
+    Racing,
+    Stealth,
+    Simulation,
+    Sports,
+    TableTop,
+    Card,
+    Other
+
+}
+
