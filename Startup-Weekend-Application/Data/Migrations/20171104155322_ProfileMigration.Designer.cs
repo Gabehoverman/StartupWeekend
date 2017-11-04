b8021f985961c189f6175133536dd3709a46ae37
Gabriel Hoverman <GabeHoverman@gabrielsmbp507.marshall.edu>
2017-11-04 12:35:55 -0400

    Added User Registration and Dashboard

--- Startup-Weekend-Application/Data/Migrations/20171104155322_ProfileMigration.Designer.cs
+++ Startup-Weekend-Application/Data/Migrations/20171104155322_ProfileMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Startup_Weekend_Application.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171104155322_ProfileMigration")]
+    partial class ProfileMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
