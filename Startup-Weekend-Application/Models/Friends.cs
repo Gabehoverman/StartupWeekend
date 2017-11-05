93d5886be1c11badb37f01fd7b284d625732664a
ZoeyOneOhOne <cole.perry113@gmail.com>
2017-11-05 12:45:27 -0500

    idk

---
+++ Startup-Weekend-Application/Models/Friends.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Startup_Weekend_Application.Models
+{
+    public class Friends
+    {
+        public int Id { get; set; }
+        public string userOne { get; set; }
+        public string userTwo { get; set; }
+        public Boolean isActive { get; set; }
+    }
+}
